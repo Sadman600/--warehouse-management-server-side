72143a740015ccb3604d29d6bf206ff36bddc649
unknown <sadmansakib600@gmail.com>
2022-05-04 00:49:34 +0600

    Added database and post data in database

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+// KF45VD5G6kJwc7er
 
 app.use(express.json());
 app.use(cors());
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.chven.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db("ITManagement").collection("item");
+        // create a document to insert
+        app.post('/item', async (req, res) => {
+            const item = req.body;
+            const result = await itemsCollection.insertOne(item);
+            res.send({ result });
+        });
+
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
